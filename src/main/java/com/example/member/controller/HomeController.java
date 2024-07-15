d9272788695200a709e1b02db7f64f943fab4202
SeungHyeok Nam <skatmdgur6365@gmail.com>
2024-07-15 21:50:40 +0900

    회원가입 및 로그인 구현

--- src/main/java/com/example/member/controller/HomeController.java
+++ src/main/java/com/example/member/controller/HomeController.java
@@ -5,9 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-    // 기본페이지 요청 메서드
     @GetMapping("/")
-    public String index(){
-        return "index"; // => templates 폴더에 index.html을 찾아감
+    public String index() {
+        return "index";
     }
 }
--- src/main/java/com/example/member/entity/MemberEntity.java
+++ src/main/java/com/example/member/entity/MemberEntity.java
@@ -1,5 +1,6 @@
 package com.example.member.entity;
 
+import com.example.member.dto.MemberDTO;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,11 +10,11 @@ import lombok.Setter;
 @Getter
 @Table(name = "member_table")
 public class MemberEntity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id // pk 지정
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // auto_increment
     private Long id;
 
-    @Column(unique = true)
+    @Column(unique = true) // unique 제약조건 추가
     private String memberEmail;
 
     @Column
@@ -21,4 +22,22 @@ public class MemberEntity {
 
     @Column
     private String memberName;
+
+    public static MemberEntity toMemberEntity(MemberDTO memberDTO) {
+        MemberEntity memberEntity = new MemberEntity();
+        memberEntity.setMemberEmail(memberDTO.getMemberEmail());
+        memberEntity.setMemberPassword(memberDTO.getMemberPassword());
+        memberEntity.setMemberName(memberDTO.getMemberName());
+        return memberEntity;
+    }
+
+    public static MemberEntity toUpdateMemberEntity(MemberDTO memberDTO) {
+        MemberEntity memberEntity = new MemberEntity();
+        memberEntity.setId(memberDTO.getId());
+        memberEntity.setMemberEmail(memberDTO.getMemberEmail());
+        memberEntity.setMemberPassword(memberDTO.getMemberPassword());
+        memberEntity.setMemberName(memberDTO.getMemberName());
+        return memberEntity;
+    }
+
 }
